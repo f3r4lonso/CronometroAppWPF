874eea5219bafc928c757f2568e703b1001c49a7
f3r4lonso <alonso.fernando.morry@gmail.com>
2022-11-25 06:48:54 +0100

    Add files via upload

---
+++ ChronosService/Services/ITimerService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace ChronosService.Services
+{
+    public interface ITimerService
+    {
+        public DispatcherTimer CreateTimer();
+    }
+}
---
+++ ChronosService/Services/TimerService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace ChronosService.Services
+{
+    public class TimerService : ITimerService
+    {
+        public DispatcherTimer CreateTimer()
+        {
+            var timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            return timer;
+        }
+    }
+}
